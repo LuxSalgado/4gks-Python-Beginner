483355c53a42b68262fd798f9968d319fa4b43f9
LuxSalgado <lasalgado.14@est.ucab.edu.ve>
2021-08-20 23:41:42 +0000

    Ejercicios resueltos hasta el número 17

--- exercises/17-Russian-Roulette/app.py
+++ exercises/17-Russian-Roulette/app.py
@@ -9,8 +9,11 @@ def spin_chamber():
 #  DON'T CHANGE THE CODE ABOVE
 def fire_gun():
 	# YOUR CODE HERE
-
-
-
+	if bullet_position == spin_chamber(): 
+		print("You are dead!")
+		return 
+	else:
+		print("Keep playing!")
+		return
 
 print(fire_gun())
