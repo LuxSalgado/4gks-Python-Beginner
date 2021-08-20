a0233962af4b9b8ab89bbe8627f772564279fcd9
LuxSalgado <lasalgado.14@est.ucab.edu.ve>
2021-08-20 23:36:40 +0000

    Ejercicios resueltos hasta el número 16

--- exercises/16-Random-Colors-Loop/app.py
+++ exercises/16-Random-Colors-Loop/app.py
@@ -3,7 +3,6 @@ import random
 def get_color(color_number=4):
     # making sure is a number and not a string
     color_number = int(color_number)
-
     switcher={
                   0:'red',
                   1:'yellow',
@@ -15,13 +14,12 @@ def get_color(color_number=4):
 
 
 def get_allStudentColors():
-
     example_color = 1
     students_array = []
     #your loop here
-
-
-
-
+    for i in range(1,11):
+        example_color = random.randint(0, 3)
+        students_array.append(get_color(example_color))
+    return students_array
 
 print(get_allStudentColors())
