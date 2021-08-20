b296a341f467be6490c72250f05f1ff1b774306a
LuxSalgado <lasalgado.14@est.ucab.edu.ve>
2021-08-20 23:47:21 +0000

    Ejercicios resueltos hasta el número 18

--- exercises/18-The-Beatles/app.py
+++ exercises/18-The-Beatles/app.py
@@ -1,1 +1,12 @@
 # Your code here!!
+def sing():
+    primero = "let it be,"
+    segundo = "whisper words of wisdom, let it be, let it be,"
+    for i in range(0,4):
+        print(primero)
+    print(segundo)
+    for i in range(0,3):
+        print("let it be,")
+    print("there will be an answer, let it be")
+
+sing()
