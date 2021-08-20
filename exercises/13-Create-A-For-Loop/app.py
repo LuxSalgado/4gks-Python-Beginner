aca67211b24b7f1b95a22c2414a0ee2c1d7dbcb8
LuxSalgado <lasalgado.14@est.ucab.edu.ve>
2021-08-20 23:05:26 +0000

    Ejercicios resueltos hasta el número 13

--- exercises/13-Create-A-For-Loop/app.py
+++ exercises/13-Create-A-For-Loop/app.py
@@ -1,4 +1,6 @@
 def standards_maker():
     #your code here
-
+    print("I will write questions if I am stuck")
 #remember to call the function outside (here)
+for seq in range(300):
+    standards_maker()
